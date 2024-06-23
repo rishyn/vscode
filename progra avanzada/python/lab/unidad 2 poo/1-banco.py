8145a57e219cc738d66ccde5df1510de9f1bd3e6
rishyn <ricarditoxd10@gmail.com>
2024-06-23 14:44:08 -0400

    actualizacion

---
+++ progra avanzada/python/lab/unidad 2 poo/1-banco.py
@@ -0,0 +1,53 @@
+
+
+
+#numero  de cuenta , titular de la cuenta , saldo de la cuenta
+
+
+class CuentaBancaria:
+
+    def __init__(self,NCuenta = 0 ,TCuenta = "" ,Saldo = 0):
+        self.NCuenta = NCuenta
+        self.TCuenta = TCuenta
+        self.Saldo = Saldo
+
+
+
+
+
+
+    pass
+
+
+
+def menu():
+    print("\nMenu Principal")
+    print("------------------------------------------")
+    print("1. Crear cuenta nueva.")
+    print("2. Hacer deposito a una cuenta.")
+    print("3. Hacer retiros a una cuenta .")
+    print("4. Imprimir el saldo de una cuenta.")
+    print("5. Imprimir el listado completa de las cuentas disponibles.")
+    print("6. salir ")
+    print("------------------------------------------")
+
+
+
+def main():
+
+    while True:
+        menu()
+        opcion = input(">>")
+        if opcion == "1":
+            
+
+
+
+    pass
+
+
+if __name__  == "__main__":
+
+    main()
+
+    pass
