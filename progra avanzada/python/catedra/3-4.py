ed22189af8a830dff193ce3aad1a6ef48cd05b1d
rishyn <ricarditoxd10@gmail.com>
2024-07-05 17:47:46 -0400

    update folder

--- progra avanzada/python/catedra/3-4.py
+++ progra avanzada/python/catedra/3-4.py
@@ -1,63 +1,78 @@
+from abc import ABC, abstractmethod
+import random
+from enum import Enum
+
+class TipoEquipo(Enum):
+    Marciano = "Marciano"
+    Terricola = "Terricola"
+
+class Guerrero(ABC):
+    """Clase abstracta que define el comportamiento de un guerrero."""
+    def __init__(self):
+        self._target = random.randint(0,9) # Inicializa un guerrero con un numero aleatorio entre 0 y 9
+        self._vivo = True # cuando se crea el gerrero esta vivo
+
+    @abstractmethod
+    def recibir_disparo(self, disparo : int):
+        """metodo abstracto q define el comportamiento recibir_disparo para cualquier guerrero"""
+        pass
+
+    def _comprobar_muerte(self):
+        """comprueba si el guerrero a muerto"""
+        if not self._vivo:
+            print(f"Guerrero muerto")
+
+    @property
+    def esta_vivo(self):
+        """devuelve si el guerrero esta vivo"""
+        return self._vivo
 
 
-"""relacion entre clases y objetos """
+class Marciano(Guerrero): # Hereda de la clase Guerrero
+    """Clase que define el comportamiento de un guerrero marciano."""
+    def __init__(self, disparos_morir=10):
+        super().__init__() # Se debe llamar al constructor de la clase padre
+        self._disparos_morir = disparos_morir # Número de disparos que debe recibir el marciano para morir. Por defecto es 10 en el argumento
 
-"""
-asociacion (conexion entre clases)
-dependencia (realcion de uso)
-generalizacion/ especializacion( herencia)
+    def recibir_disparo(self, disparo: int):
+        """
+            Recibe un disparo y actualiza el estado del marciano.
 
-"""
+            Args:
+                disparo: Número del disparo recibido.
+        """
+        if disparo == self._target:
+            self._disparos_morir -= 1
 
-# Relacion de asociación entre clases mediante clase asociativa
-class Cliente:
-  def __init__(self, nombreCompleto, rut, direccion, clienteVIP):
-    self.nombreCompleto = nombreCompleto
-    self.rut = rut
-    self.direccion = direccion
-    self.clienteVIP = clienteVIP
+        if self._disparos_morir <= 0:
+            self._vivo = False
 
-class Libro:
-  def __init__(self, idLibro, titulo, autor, precioBase, cantidadStock, numeroPaginas, bestSeller):
-    self.idLibro = idLibro
-    self.titulo = titulo
-    self.autor = autor
-    self.precioBase = precioBase
-    self.cantidadStock = cantidadStock
-    self.numeroPaginas = numeroPaginas
-    self.bestSeller = bestSeller
+        self._comprobar_muerte()
 
-class OrdenVenta:
-  def __init__(self, rutCliente, idLibro, cantidad):
-    self.rutCliente = rutCliente # referencia a instancia de Cliente mediante rutCliente
-    self.idLibros = [idLibro] # referencia a instancia de Libro mediante idLibro
-    self.cantidadLibros = [cantidad]
+    def __str__(self):
+        return f"Marciano {self._vivo} con {self._disparos_morir} disparos para morir"
+
+
+class Terricola(Guerrero): # Hereda de la clase Guerrero
+    """Clase que define el comportamiento de un guerrero terrícola."""
+    def __init__(self, salud=5):
+        super().__init__() # Se debe llamar al constructor de la clase padre
+        self._salud = salud # Salud del terrícola. Por defecto es 5 en el argumento
+
+    def recibir_disparo(self, disparo: int):
+        """
+            Recibe un disparo y actualiza el estado del terrícola.
+
+            Args:
+                disparo: Número del disparo recibido.
+        """
+        if disparo == self._target+1 or disparo == self._target-1: # Condicion (+-)1 para disminuir salud. Equivalente: disparo+1 == self._target or disparo-1 == self._target:
+            self._salud -= 1
+
+        if self._salud <= 0:
+            self._vivo = False
+
+        self._comprobar_muerte()
 
   def __str__(self):
-    return f"Cliente: {self.rutCliente}, Libro: {self.idLibros}, Cantidad: {self.cantidadLibros}"
-
-  def __del__(self):
-    print("Orden de venta eliminada")
-
-  def adicionarLibro(self, idLibro, cantidad):
-    self.idLibros.append(idLibro)
-    self.cantidadLibros.append(cantidad)
-
-
-
-cliente_1 = Cliente("Pedro Perez", "12345678-9", "Calle 123", True)
-cliente_2 = Cliente("Maria Rodriguez", "98765432-1", "Avenida 456", False)
-
-libro_1 = Libro(1, "Libro 1", "Autor 1", 10.0, 10, 300, False)
-libro_2 = Libro(2, "Libro 2", "Autor 2", 15.0, 5, 400, True)
-
-orden_venta_1 = OrdenVenta("12345678-9", 1, 2)
-orden_venta_1.adicionarLibro(2, 3)
-
-orden_venta_2 = OrdenVenta("98765432-1", 2, 1)
-
-print(orden_venta_1)
-print(orden_venta_2)
-
-del orden_venta_1 # Elimina la instancia de OrdenVenta
-print(cliente_1) # No se eliminan las instancias referenciadas en orden_venta_1
+    return f"Terricola {self._vivo} con {self._salud} de salud"
--- progra avanzada/python/lab/unidad 1/0-experimentos.py
+++ progra avanzada/python/lab/unidad 1/0-experimentos.py
@@ -1,35 +1,14 @@
-def calcular_total_con_descuento(precios, descuento):
-    total = sum(precios)
-    descuento_porcentaje = descuento / 100
-    descuento_monto = total * descuento_porcentaje
-    total_con_descuento = total - descuento_monto
-    return total, total_con_descuento
+from enum import Enum
 
-# Solicitar precios al usuario
-precios = []
-precio = float(input("Ingrese los precios de los artículos (ingrese 0 para finalizar): "))
+class DiaSemana(Enum):
+    LUNES = 1
+    MARTES = 2
+    MIERCOLES = 3
+    JUEVES = 4
+    VIERNES = 5
+    SABADO = 6
+    DOMINGO = 7
 
-while precio != 0:
-
-    precios.append(precio)
-    precio = float(input("Ingrese los precios de los artículos (ingrese 0 para finalizar): "))
-
-# Solicitar descuento al usuario
-descuento_valido = False
-while not descuento_valido:
-    descuento = input("Ingrese el descuento (en porcentaje, máximo 50%): ")
-    if descuento.replace('.', '', 1).isdigit():
-        descuento = float(descuento)
-        if 0 <= descuento <= 50:
-            descuento_valido = True
-        else:
-            print("El descuento debe ser un valor entre 0% y 50%.")
-    else:
-        print("Ingrese un valor numérico válido.")
-
-# Calcular total con descuento
-total, total_con_descuento = calcular_total_con_descuento(precios, descuento)
-
-# Imprimir resultado
-print(f"El total a pagar sin descuento es: ${total:.2f}")
-print(f"El total a pagar con un descuento del {descuento}% es: ${total_con_descuento:.2f}")
+# Iterar sobre los miembros de la enumeración
+for dia in DiaSemana:
+    print(dia.name, dia.value)
