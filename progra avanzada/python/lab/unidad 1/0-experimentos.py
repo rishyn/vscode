68d73688e67c6ba6676b828c29364ab14f99dbba
rishyn <ricarditoxd10@gmail.com>
2024-11-27 10:24:23 -0300

    Update 0-experimentos.py

--- progra avanzada/python/lab/unidad 1/0-experimentos.py
+++ progra avanzada/python/lab/unidad 1/0-experimentos.py
@@ -1,14 +1,10 @@
-from enum import Enum
 
-class DiaSemana(Enum):
-    LUNES = 1
-    MARTES = 2
-    MIERCOLES = 3
-    JUEVES = 4
-    VIERNES = 5
-    SABADO = 6
-    DOMINGO = 7
 
-# Iterar sobre los miembros de la enumeración
-for dia in DiaSemana:
-    print(dia.name, dia.value)
+
+precioT = int(input("Ingrese el precio total: "))
+
+precioC = int(input("Ingresar precio a calcular porcentaje: "))
+
+porcentaje = int((precioC * 100) / precioT)
+
+print("El precio total es:", precioT ,"precio 4 procentaje: ", precioC, "equivale a %: ", porcentaje)
